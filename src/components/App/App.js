ca98a281772c0713a972378c2d9209a5d3a88232
DaoNik <nikitataranin@gmail.com>
2022-03-26 18:00:23 +0300

    fix: удалил лишний импорт

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
