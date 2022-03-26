1728eb277f8154f4bb17950a436d7f91836174a5
DaoNik <nikitataranin@gmail.com>
2022-03-26 17:59:27 +0300

    fix: поменял стили для Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,33 +5,38 @@ import profile from '../../images/profile.svg';
 
 function Navigation({ isOpen, closeSidebar }) {
   return (
-    <section className={`sidebar ${isOpen ? 'is-open' : ''}`}>
-      <NavLink to='/' className='sidebar__link link'>
-        Главная
-      </NavLink>
-      <NavLink to='/movies' className='sidebar__link sidebar__link_active link'>
-        Фильмы
-      </NavLink>
-      <NavLink to='/saved-movies' className='sidebar__link link'>
-        Сохранённые фильмы
-      </NavLink>
-      <NavLink
-        to='/profile'
-        className='sidebar__link sidebar__link_value-acc link'
-      >
-        Аккаунт
-        <img
-          className='sidebar__img-profile'
-          src={profile}
-          alt='изображение фигуры человека'
-        />
-      </NavLink>
-      <button
-        onClick={closeSidebar}
-        className='sidebar__btn'
-        type='button'
-      ></button>
-    </section>
+    <div className={`sidebar ${isOpen ? 'sidebar_opened' : ''}`}>
+      <section className='sidebar__container'>
+        <NavLink to='/' className='sidebar__link link'>
+          Главная
+        </NavLink>
+        <NavLink
+          to='/movies'
+          className='sidebar__link sidebar__link_active link'
+        >
+          Фильмы
+        </NavLink>
+        <NavLink to='/saved-movies' className='sidebar__link link'>
+          Сохранённые фильмы
+        </NavLink>
+        <NavLink
+          to='/profile'
+          className='sidebar__link sidebar__link_value-acc link'
+        >
+          Аккаунт
+          <img
+            className='sidebar__img-profile'
+            src={profile}
+            alt='изображение фигуры человека'
+          />
+        </NavLink>
+        <button
+          onClick={closeSidebar}
+          className='sidebar__btn'
+          type='button'
+        ></button>
+      </section>
+    </div>
   );
 }
 
