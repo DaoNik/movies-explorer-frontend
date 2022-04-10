f4f3ec2b1c6c58eb07530f838dfc511aaf9898e2
DaoNik <nikitataranin@gmail.com>
2022-04-10 22:01:17 +0300

    feed: добавил фильтрацию реагирующую на размер экрана и поправил работу кнопки Ещё

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 function MoviesCardList({ movies, saveMovie, deleteMovie, isSearchMovies }) {
   const windowInnerWidth = window.innerWidth;
+  const [arrMovies, setArrMovies] = useState([]);
+  const [addMovie, setAddMovie] = useState(0);
+
+  function handleAddMovies() {
+    const result = [];
+    if ( movies.length - arrMovies.length < addMovie ) {
+      for (let i = arrMovies.length; i < movies.length; i++) {
+        result.push(movies[i])
+      }
+    } else {
+      for (let i = arrMovies.length; i < arrMovies.length + addMovie; i++) {
+        result.push(movies[i])
+      }
+    }
+    setArrMovies([...arrMovies, ...result]);
+    if (arrMovies.length === movies.length) {
+      setAddMovie(0);
+    }
+  }
+
+  useEffect(() => {
+    const result = [];
+    if (movies.length > 12 && windowInnerWidth >= 1280) {
+      for (let i = 0; i < 12; i++) {
+        result.push(movies[i])
+      }
+      setAddMovie(3);
+    } else if (movies.length > 8 && windowInnerWidth >= 768) {
+      for (let i = 0; i < 8; i++) {
+        result.push(movies[i])
+      }
+      setAddMovie(2)
+    } else if (movies.length > 5 && windowInnerWidth >= 320 && windowInnerWidth < 768) {
+      for (let i = 0; i < 5; i++) {
+        result.push(movies[i])
+      }
+      setAddMovie(1);
+    } else if (movies.length) {
+     result.push(...movies);
+    } else {
+      setAddMovie(0);
+    }
+    setArrMovies(result);
+  }, [movies])
 
   return (
     <section className='gallery'>
-      <ul className='gallery__list'>{movies.length ? movies.map((movie) => {
+      <ul className='gallery__list'>{arrMovies.length ? arrMovies.map((movie) => {
         return (
           <MoviesCard
             saved={false}
@@ -19,7 +63,7 @@ function MoviesCardList({ movies, saveMovie, deleteMovie, isSearchMovies }) {
           />
         )
       }) : `${isSearchMovies ? 'К сожалению, ничего не найдено' : ''}`}</ul>
-      {movies.length > 3 ?<button className='gallery__button link'>Ещё</button> : ''}
+      {addMovie ?<button className='gallery__button link' onClick={handleAddMovies}>Ещё</button> : ''}
     </section>
   );
 }
